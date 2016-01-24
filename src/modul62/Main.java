f0f2228c23f632c50fa67206004a966cb3da2fe1
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-24 13:17:06 +0200

    DZ Modul 6 Array

--- src/modul62/Main.java
+++ src/modul62/Main.java
@@ -2,7 +2,7 @@ package modul62;
 
 public class Main {
 
-    public void arrayToDirectory{
+    public void arrayToDirectory(){
         File[] fileInDirectory = {new File(), new Picture(), new Audio(), new Text(), new Video(), new Directory()};
     }
 
